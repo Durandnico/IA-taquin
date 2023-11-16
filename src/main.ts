e387fefc31f4006743189d3e68601a98fffbd66e
durandnico <nicod4956@gmail.com>
2023-11-16 15:42:47 +0100

    interactive menu

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import Taquin from "./Taquin.ts"
+import { manhanttan, hamming } from "./utils/heuristic.ts";
 import inquirer from "inquirer";
+
+const menu = await inquirer.prompt([
+  {
+    type: 'list',
+    name: 'choice',
+    message: 'Mode de jeu ?',
+    choices : ['IA', 'Manuel']
+  }
+]);
+
 
 async function setup() {
   let game : Taquin = new Taquin();
 
   game.render();
-
+  
   while(!game.isWin())
   {
     const answer = await inquirer.prompt([
@@ -15,12 +26,44 @@ async function setup() {
         message: 'What move would you like to play ? '
       }
     ]);
+
     try {
+
       if(!game.move(answer.move))
         console.warn("Impossible move");
-    } catch (error) {
-    }
+    
+    } catch (error){}
   }
 }
 
-setup();
+if(menu.choice === "Manuel")
+  setup();
+else {
+  const functionChoice = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'fctH',
+      message: 'Quelle fonction heuristique utiliser ?',
+      choices: ['Hamming', 'Manhanttan']
+    }
+  ]);
+
+  let fctH : Function | null = null;
+  switch(functionChoice.fctH)
+  {
+    case 'Hamming':
+      fctH = hamming;
+      break;
+    
+    case 'Manhanttan':
+      fctH = manhanttan;
+      break;
+    
+    default:
+      console.error("Error lors du choix de la fonction");
+  }
+
+  console.log(fctH)
+  //if(fctH !== null);
+    //solver(fctH);
+}
