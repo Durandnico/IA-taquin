3986c49aa9759f38d0fd5a605b81f805cba600bf
durandnico <nicod4956@gmail.com>
2023-11-24 14:34:21 +0100

    new menu + duel

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import Taquin from "./Taquin.ts"
 import { manhanttan, hamming } from "./utils/heuristic.ts";
+import Pair from "./utils/Pair.ts";
+import TimeDiff from "./utils/TimeDiff.ts"; 
 import inquirer from "inquirer";
+import Astar from "./solver.ts";
 
 const menu = await inquirer.prompt([
   {
@@ -12,58 +15,88 @@ const menu = await inquirer.prompt([
 ]);
 
 
-async function setup() {
+async function setupManuel() {
   let game : Taquin = new Taquin();
 
   game.render();
-  
+  new TimeDiff(game.scramble, Taquin);
   while(!game.isWin())
   {
-    const answer = await inquirer.prompt([
-      {
-        type: 'number',
-        name: 'move',
-        message: 'What move would you like to play ? '
-      }
-    ]);
-
-    try {
-
-      if(!game.move(answer.move))
-        console.warn("Impossible move");
-    
-    } catch (error){}
+    await game.promptMove();
+    game.render();
   }
 }
 
-if(menu.choice === "Manuel")
-  setup();
-else {
+
+async function setupIa() {
   const functionChoice = await inquirer.prompt([
     {
       type: 'list',
       name: 'fctH',
       message: 'Quelle fonction heuristique utiliser ?',
-      choices: ['Hamming', 'Manhanttan']
+      choices: ['Hamming', 'Manhanttan', 'Duel']
     }
   ]);
 
   let fctH : Function | null = null;
+  let fctName: string = "";
   switch(functionChoice.fctH)
   {
     case 'Hamming':
       fctH = hamming;
+      fctName = "Hamming"
       break;
     
     case 'Manhanttan':
       fctH = manhanttan;
+      fctName = "Manhanttan"
       break;
-    
+
     default:
-      console.error("Error lors du choix de la fonction");
+
   }
 
-  console.log(fctH)
-  //if(fctH !== null);
-    //solver(fctH);
+  console.log(fctH);
+  let game : Taquin = new Taquin([
+    [4,2,1],
+    [7,0,3],
+    [8,6,5]
+  ]);
+  game.scramble()
+  if(fctH !== null) {
+    const elapse = new TimeDiff<Pair<number, number[]>>(Astar, fctH, game);
+    const result = elapse.value;
+    
+    console.clear();
+    console.log('  ============== RESULT : ============== ');
+
+    console.log('test Grid :');
+    game.draw();
+
+    console.log('\t '+ fctName +' :\n\t\t- solution find in '+ elapse.time + 'ms\n\t\t- number of move check: ' + result.first + "\n\t\t- solution in " + result.second.length + " moves which are :\n\t\t" + result.second);
+  }
+    
+  else {
+    
+    const elapseHmg = new TimeDiff<Pair<number, number[]>>(Astar, hamming, game);
+    const hmg = elapseHmg.value;
+
+    const elapseHmt = new TimeDiff<Pair<number, number[]>>(Astar, manhanttan, game)
+    const mht:Pair<number, number[]> = elapseHmt.value
+
+    console.clear();
+    console.log('  ============== RESULT : ============== ');
+
+    console.log('Test Grid :');
+    game.draw();
+
+    console.log('\t Hamming :\n\t\t- solution find in '+ elapseHmg.time + 'ms\n\t\t- number of move check: ' + hmg.first + "\n\t\t- solution in " + hmg.second.length + " moves which are :\n\t\t" + hmg.second);
+    console.log("\n\t Manhanttan :\n\t\t- solution find in "+ elapseHmt.time + "ms\n\t\t- number of move check: " + mht.first + "\n\t\t- solution in " + mht.second.length + " moves which are :\n\t\t" + mht.second+ "\n\n\t\t\t---------------------------\n"); 
+  }
+}
+
+if(menu.choice === "Manuel")
+  setupManuel();
+else {
+  setupIa();
 }
